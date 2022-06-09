1e95ec27aa42bb928b336c7c18d6049f52ab4ac9
Jouannatadros <jouanna.tadros@student.giu-uni.de>
2022-06-09 12:24:36 +0200

    tut

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-const bodyParser = require("body-parser");
-const query = require("./queries");
-const req = require("express/lib/request");
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser')
+const query  = require('./queries');
+const req = require('express/lib/request');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
+)
+
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   // Pass to next layer of middleware
-  next();
+  next(); 
 });
 
-app.get("/products", query.getproducts);
+
+app.get('/products', query.getproducts);
 
 // app.post('/checkout', query.create_chekout_session);
 
-app.post("/addOrder", query.addOrder);
+app.post('/addOrder', query.addOrder);
 
 //app.post('/error:id', query.cancelOrder);
 
 //app.post('/success:id', query.fulfilOrder);
 
+
 app.listen(3000, () => {
-  console.log(`App running on port 3000.`);
-});
+  console.log(`App running on port 3000.`)
+})
